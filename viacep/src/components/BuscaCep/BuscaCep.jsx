20534f9f43164b4944334e10b901bd3b4ac97aa2
brendatondin <bentotbrenda@gmail.com>
2022-08-18 20:09:08 -0300

    impletação da busca de cep da api

--- viacep/src/components/BuscaCep/BuscaCep.jsx
+++ viacep/src/components/BuscaCep/BuscaCep.jsx
@@ -3,21 +3,44 @@ import { useState } from 'react'
 
 const BuscaCep = () => {
 
-    // const [cepInput, setCepInput] = useState(01001000)
+  const [cepInput, setCepInput] = useState('01001000')
 
-    async function handleBuscaCep(){
+  function handleSetInput(target) {
+    setValorInput(target.value)
+  }
 
-        const cepInput = '01001000'
+  async function handleBuscaCep() {
 
-        const url = 'https://viacep.com.br/ws/' + cepInput + '/json/'
-        const response = await fetch(url)
-        const json = response.json()
-        console.log(json);
-        
+    const url = 'https://viacep.com.br/ws/' + cepInput + '/json/'
+    const response = await fetch(url)
+    const json = response.json()
+    console.log(json);
+
+    const resposta = {
+      bairro: json.bairro,
+      cep: json.cep,
+      complemento: json.complemento,
+      localidade: json.localidade,
+      logradouro: json.logradouro,
+      uf: json.uf
     }
+    console.log(resposta);
+    // setEndereco(resposta)
+    setCepInput('')
+
+  }
 
   return (
-    <div><button onClick={handleBuscaCep}></button></div>
+    <div>
+      <input
+          type="text"
+          // className={S.input}
+          placeholder='Digite o CEP'
+          value={cepInput}
+          onChange={({ target }) => handleSetInput(target)}
+        />
+      <button onClick={handleBuscaCep}>Enviar</button>
+    </div>
   )
 }
 
